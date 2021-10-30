a0414f5efb4608931c3120c6a74b8492cfd9b9b9
Bhaskar Sen <senbhaskar26@gmail.com>
2021-10-31 04:43:06 +0530

    feat: webcam live streaming

--- app.py
+++ app.py
@@ -1,7 +1,34 @@
-from flask import Flask
+from flask import Flask, Response, render_template
+import cv2
 
 app = Flask(__name__)
 
-@app.route("/")
+camera = cv2.VideoCapture(0)  # Local Webcam
+
+# Generating the frames for Streaming
+def gen_frames():
+    while True:
+        success, frame = camera.read()  # Reading the camera frame
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+
+
+@app.route('/video_feed')
+def video_feed():
+    #Video streaming route
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+@app.route('/')
 def index():
-    return "Hello World!"
+    """Webcam Streaming"""
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=False)
